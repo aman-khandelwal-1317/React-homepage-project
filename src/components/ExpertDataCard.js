7c1c313859244cf18bc40e18a79099a1829e7887
aman-khandelwal-1317 <amankhandelwal1500@gmail.com>
2020-10-28 08:42:47 +0530

    Commit 4 : beautified files and created build for production deployment

--- src/components/ExpertDataCard.js
+++ src/components/ExpertDataCard.js
@@ -1,34 +1,49 @@
-import React from "react" ;
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome' ;
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const ExpertDataCard = ({expertData}) => {
-    return (
-        <div class="card expert-info">
-        <div class="left-div">
-            <img src={expertData.img_src}></img>
-             <h6>Price : <span className="price-span">{expertData.price}</span></h6>
-             <p> {expertData.rating}  ({expertData.reviews})</p>
+const ExpertDataCard = ({ expertData }) => {
+  return (
+    <div class="card expert-info">
+      <div class="left-div">
+        <img src={expertData.img_src}></img>
+        <h6>
+          Price : <span className="price-span">{expertData.price}</span>
+        </h6>
+        <p>
+          {" "}
+          {expertData.rating} ({expertData.reviews})
+        </p>
+      </div>
+      <div class="right-div">
+        <h5>{expertData.name}</h5>
+        <h6>
+          Founder @ {expertData.company} CEO @{expertData.ceo}
+        </h6>
+        <hr></hr>
+        <div className="span-div">
+          <span className="card-span">
+            {" "}
+            <FontAwesomeIcon icon="clock" /> {expertData.free_sessions} free
+            sessions available
+          </span>
+          <span className="card-span">
+            <FontAwesomeIcon icon="map-marker-alt" /> {expertData.location}
+          </span>
+          <span className="card-span">
+            <FontAwesomeIcon icon="font" /> {expertData.language_known}
+          </span>
         </div>
-        <div class="right-div">
-             <h5 >{expertData.name}</h5>
-    <h6>Founder @ { expertData.company} CEO @{expertData.ceo}</h6>
-             <hr></hr>
-             <div className="span-div">
-             <span className="card-span"> <FontAwesomeIcon icon="clock" /> {expertData.free_sessions} free sessions available</span>
-             <span className="card-span"><FontAwesomeIcon icon="map-marker-alt" />  {expertData.location}</span>
-            <span className="card-span"><FontAwesomeIcon icon="font" />  {expertData.language_known}</span>
-             </div>
-            
-           <p> {expertData.description}</p>
-     <div class="btn-container">
-            <button class="btn btn-light btn-desc">Marketing</button>
-            <button class="btn btn-light btn-desc">SEO</button>
-            <button class="btn btn-light btn-desc">Marketing</button>
-            <button class="btn btn-light btn-desc">Marketing</button>
-     </div>
+
+        <p> {expertData.description}</p>
+        <div class="btn-container">
+          <button class="btn btn-light btn-desc">Marketing</button>
+          <button class="btn btn-light btn-desc">SEO</button>
+          <button class="btn btn-light btn-desc">Marketing</button>
+          <button class="btn btn-light btn-desc">Marketing</button>
         </div>
       </div>
-    )
-}
+    </div>
+  );
+};
 
-export default ExpertDataCard ;
+export default ExpertDataCard;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,179 +1,217 @@
-import React from "react" ;
-import ExpertDataCard from "../components/ExpertDataCard" ;
-import data from "../components/data/ExpertData.json" ;
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome' ;
+import React from "react";
+import ExpertDataCard from "../components/ExpertDataCard";
+import data from "../components/data/ExpertData.json";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-class HomePage extends React.Component{
+class HomePage extends React.Component {
+  renderExpertData(data) {
+    const singleData = data.map((expertData) => {
+      return <ExpertDataCard expertData={expertData} />;
+    });
+    return singleData;
+  }
 
+  render() {
+    return (
+      <div id="bodydiv">
+        <header>
+          <nav className="main-nav">
+            <div className="logo navbar-brand">
+              <a href=""> Logo</a>
+            </div>
+            <div className="options">
+              <a className="dashboard " href="/">
+                Dashboard
+              </a>
+              <a className="expert active" href="/">
+                Find Experts
+              </a>
+              <a className="discussion" href="/">
+                Discussion
+              </a>
+            </div>
+            <div className="icons">
+              <a className="ic1" href="/">
+                <FontAwesomeIcon icon="bell" />
+              </a>
+              <a className="ic2" href="">
+                <FontAwesomeIcon icon="envelope" />
+              </a>
+            </div>
+          </nav>
+        </header>
 
-    renderExpertData(data){
-        const singleData =  data.map((expertData) => {
-            return (
-             
-               < ExpertDataCard expertData = {expertData}/>
-             
-            )
-          })
-          return singleData ;
-        }
-    
-    render() {
+        <div id="main">
+          <aside className="aside-container">
+            <div className="filters">
+              <ul>
+                <h5>Filters</h5>
+              </ul>
+            </div>
 
-        return (
-
-            <div id="bodydiv">
-             <header>
-             <nav class="main-nav">
-
-                  <div class="logo navbar-brand">
-                  <a href=""> Logo</a>
+            <div className="card">
+              <div className="aside-card">
+                <div className="expertise">
+                  <h6>Expertise</h6>
+                  <div>
+                    <select
+                      className="selectpicker"
+                      data-live-search="true"
+                      multiple
+                      title="Search here ..."
+                      data-width="250px"
+                      data-selected-text-format="count > 2"
+                    >
+                      <option data-content="<span className='badge badge-success'>Relish</span>">
+                        Branding1
+                      </option>
+                      <option data-content="<span className='badge badge-success'>Relish</span>">
+                        Branding2
+                      </option>
+                      <option data-content="<span className='badge badge-success'>Relish</span>">
+                        Branding3
+                      </option>
+                    </select>
                   </div>
-                   <div class="options">
-                   <a className="dashboard " href="/">Dashboard</a>
-                   <a className="expert active" href="/">Find Experts</a>
-                  <a className="discussion" href="/">Discussion</a>
-                   </div>
-                   <div class="icons">
-                   <a className="ic1" href="/"><FontAwesomeIcon  icon="bell" /></a>
-              <a className="ic2" href=""><FontAwesomeIcon  icon="envelope" /></a>
-                   </div>
-    
-           
-             </nav>
-             </header>
-            
-             <div id="main">
-               <aside className="aside-container">        
-                    
-                 <div className="filters">
-                     <ul>
-                         <h5>
-                         Filters
-                         </h5>
-                     </ul>
-                
-                 </div>
-                
-                 <div className="card" >
-                      <div className="aside-card">
-                    <div className="expertise">
-                        <h6>Expertise</h6>
-                    <div>
-                   <select 
-                   class="selectpicker" 
-                   data-live-search="true" 
-                   multiple title="Search here ..." 
-                   data-width="250px"
-                   data-selected-text-format="count > 2">
-                      
-                  <option data-content="<span class='badge badge-success'>Relish</span>">Branding1</option>
-                  <option data-content="<span class='badge badge-success'>Relish</span>">Branding2</option>
-                  <option data-content="<span class='badge badge-success'>Relish</span>">Branding3</option>
-                  </select>
-                   </div>
-                    </div>
-                  
+                </div>
 
-                     <div className="focus-area">
-                         <h6>Focus Area</h6>
-                     <div>
-                   <select 
-                   class="selectpicker" 
-                   data-live-search="true" 
-                   multiple title="Search here ..." 
-                   data-width="250px"
-                   data-selected-text-format="count > 2">
-                      
-                  <option data-content="<span class='badge badge-success'>Relish</span>">Focusarea</option>
-                  <option data-content="<span class='badge badge-success'>Relish</span>">Focusarea</option>
-                  <option data-content="<span class='badge badge-success'>Relish</span>">Focusarea</option>
-                  </select>
-                   </div>
-                     </div>
-                  
-                     <div className="ratings">
-                    <h6>Ratings</h6>
+                <div className="focus-area">
+                  <h6>Focus Area</h6>
+                  <div>
+                    <select
+                      className="selectpicker"
+                      data-live-search="true"
+                      multiple
+                      title="Search here ..."
+                      data-width="250px"
+                      data-selected-text-format="count > 2"
+                    >
+                      <option data-content="<span className='badge badge-success'>Relish</span>">
+                        Focusarea
+                      </option>
+                      <option data-content="<span className='badge badge-success'>Relish</span>">
+                        Focusarea
+                      </option>
+                      <option data-content="<span className='badge badge-success'>Relish</span>">
+                        Focusarea
+                      </option>
+                    </select>
+                  </div>
+                </div>
+
+                <div className="ratings">
+                  <h6>Ratings</h6>
+                  <div>
                     <div>
-                        <div>
-                        <input className="ratings-checkbox" type="checkbox"></input>
-                          <span className="star-span"><FontAwesomeIcon icon="star" /></span>
-                          <span className="star-span"><FontAwesomeIcon icon="star" /></span>
-                          <span className="star-span"><FontAwesomeIcon icon="star" /></span>
-                          <span className="star-span"><FontAwesomeIcon icon="star" /></span>
-                         <input className="ratings-checkbox" type="checkbox"></input>
-                         <span className="star-span"><FontAwesomeIcon icon="star" /></span>
-                         <span className="star-span"><FontAwesomeIcon icon="star" /></span>
-                         <span className="star-span"><FontAwesomeIcon icon="star" /></span>
-                        </div>
-                 <div>
-                 <input className="ratings-checkbox" type="checkbox"></input>
-                 <span className="star-span"><FontAwesomeIcon icon="star" /></span>
-                         <span className="star-span"><FontAwesomeIcon icon="star" /></span>
-                 <input className="ratings-checkbox" type="checkbox"></input>
-                 <span className="star-span"><FontAwesomeIcon icon="star" /></span>
-                 </div>
-                 
-                    
+                      <input
+                        className="ratings-checkbox"
+                        type="checkbox"
+                      ></input>
+                      <span className="star-span">
+                        <FontAwesomeIcon icon="star" />
+                      </span>
+                      <span className="star-span">
+                        <FontAwesomeIcon icon="star" />
+                      </span>
+                      <span className="star-span">
+                        <FontAwesomeIcon icon="star" />
+                      </span>
+                      <span className="star-span">
+                        <FontAwesomeIcon icon="star" />
+                      </span>
+                      <input
+                        className="ratings-checkbox"
+                        type="checkbox"
+                      ></input>
+                      <span className="star-span">
+                        <FontAwesomeIcon icon="star" />
+                      </span>
+                      <span className="star-span">
+                        <FontAwesomeIcon icon="star" />
+                      </span>
+                      <span className="star-span">
+                        <FontAwesomeIcon icon="star" />
+                      </span>
                     </div>
+                    <div>
+                      <input
+                        className="ratings-checkbox"
+                        type="checkbox"
+                      ></input>
+                      <span className="star-span">
+                        <FontAwesomeIcon icon="star" />
+                      </span>
+                      <span className="star-span">
+                        <FontAwesomeIcon icon="star" />
+                      </span>
+                      <input
+                        className="ratings-checkbox"
+                        type="checkbox"
+                      ></input>
+                      <span className="star-span">
+                        <FontAwesomeIcon icon="star" />
+                      </span>
+                    </div>
+                  </div>
                 </div>
 
                 <div className="price">
-                    <h6>Price</h6>
+                  <h6>Price</h6>
+                  <div>
                     <div>
-                 <div>
-                 <input className="ratings-checkbox" type="checkbox"></input>
-                 <span>Free</span>
-                 </div>
-                <div>
-                <input className="ratings-checkbox" type="checkbox"></input>
-                <span>Less than $25</span>
-                </div>
-               <div>
-               <input className="ratings-checkbox" type="checkbox"></input>
-                <span>$25-$50</span>
-               </div>
-                <div>
-                <input className="ratings-checkbox"  type="checkbox"></input>
-                <span>$50-$100</span>
-                </div>
-                <div>
-                <input className="ratings-checkbox" type="checkbox"></input>
-                 <span>More than $100</span>
-                </div>
-      
-    
+                      <input
+                        className="ratings-checkbox"
+                        type="checkbox"
+                      ></input>
+                      <span>Free</span>
                     </div>
+                    <div>
+                      <input
+                        className="ratings-checkbox"
+                        type="checkbox"
+                      ></input>
+                      <span>Less than $25</span>
+                    </div>
+                    <div>
+                      <input
+                        className="ratings-checkbox"
+                        type="checkbox"
+                      ></input>
+                      <span>$25-$50</span>
+                    </div>
+                    <div>
+                      <input
+                        className="ratings-checkbox"
+                        type="checkbox"
+                      ></input>
+                      <span>$50-$100</span>
+                    </div>
+                    <div>
+                      <input
+                        className="ratings-checkbox"
+                        type="checkbox"
+                      ></input>
+                      <span>More than $100</span>
+                    </div>
+                  </div>
                 </div>
-
-   
-    
-    
-</div>
-
-</div>
-                
-                 
-             </aside>
-             <article className="article"> 
-                 <div class="article-title">
-                    
-                        <h5>Featured Experts</h5>
-                        <button class="btn btn-light  border border-dark">Best Match</button>
-                    
-                 </div>
-                
-                 {
-            this.renderExpertData(data)
-                 }
-               
-                
-                 </article>   
+              </div>
             </div>
-          </div>
-            
-        )
-    }
+          </aside>
+          <article className="article">
+            <div className="article-title">
+              <h5>Featured Experts</h5>
+              <button className="btn btn-light  border border-dark">
+                Best Match
+              </button>
+            </div>
 
+            {this.renderExpertData(data)}
+          </article>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default HomePage ;
+export default HomePage;
